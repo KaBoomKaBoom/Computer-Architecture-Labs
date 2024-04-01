8d2919331957b306faa5f57b972bdb6349c9e018
Berco Andrei <andrei2003berco@gmail.com>
2024-04-02 00:14:21 +0300

    add ex2 and ex3

--- Lab4/ex1.asm
+++ Lab4/ex1.asm
@@ -1,26 +1,18 @@
-global main       ; make the label _start visible externally
- 
-extern WriteFile        ; include the WriteFile function
-extern GetStdHandle     ; include the GetStdHandle function
- 
-section .data   ; data section
-message: db "Hello! This is my first program!",10  ; string to output to console
- 
-section .text       ; code section declaration
-main:             ; _start label - entry point of the program
-    sub  rsp, 40   ; Allocate space on the stack for function parameters (40 bytes)
-    mov  rcx, -11  ; Set the argument for GetStdHandle to STD_OUTPUT (-11)
-    call GetStdHandle ; Call the GetStdHandle function to obtain a handle to the standard output
-    mov  rcx, rax     ; Set the file descriptor (handle) for WriteFile to the handle obtained from GetStdHandle
-    mov  rdx, message    ; Set the pointer to the message to be written to the console
-    mov  r8d, 35      ; Set the length of the message string to be written
-    xor  r9, r9       ; Initialize the parameter for WriteFile (used to store the number of bytes written)
-    mov  qword [rsp + 32], 0  ; Initialize the fifth parameter for WriteFile (used to store the number of bytes written)
-    call WriteFile ; Call the WriteFile function to write the message to the console
-    add  rsp, 40 ; Deallocate the space allocated on the stack for function parameters
-    ret             ; exit the program
+section .data
+    hello db 'Hello, World!', 10  ; String to be printed, with newline character (10)
 
+section .text
+    global _start   ; Entry point for the program
 
+_start:
+    ; System call to write to standard output (file descriptor 1)
+    mov rax, 1      ; syscall number for sys_write
+    mov rdi, 1      ; file descriptor 1 (stdout)
+    mov rsi, hello  ; pointer to the string
+    mov rdx, 13     ; length of the string
+    syscall         ; invoke the system call
 
-
-
+    ; System call to exit the program
+    mov rax, 60     ; syscall number for sys_exit
+    xor rdi, rdi    ; exit code 0
+    syscall         ; invoke the system call
--- Lab4/ex2.asm
+++ Lab4/ex2.asm
@@ -1,62 +1,50 @@
-; Program to add two numbers
-section .text
-	global main
-main:
-	; handles the first input
-	mov eax, 4; write
-	mov ebx, 1; std_out
-	mov ecx, [rel msg1]; what to write
-	mov edx, msg1_len
-	int 0x80; sys_call
-	; perform computations
-	mov eax, 3; grab data from keyboard
-	mov ebx, 0;
-	mov ecx, [rel num1];
-	mov edx, 5; remember our 5 bytes
-	int 0x80
-	; handles the second input
-	mov eax, 4; write
-	mov ebx, 1; std_out
-	mov ecx, [rel msg2]; what to write
-	mov edx, msg2_len
-	int 0x80
-	; perform computations
-	mov eax, 3; grab data from keyboard
-	mov ebx, 0;
-	mov ecx, [rel num2];
-	mov edx, 5; remember our 5 bytes
-	int 0x80
-	mov eax, 4
-	mov ebx, 1
-	mov ecx, [rel msg]
-	mov edx, msg_len
-	int 0x80
-    ; write final output
-    movzx eax, byte [rel num1] ; Load ASCII character of num1 into AL and zero-extend to EAX
-    sub eax, '0'           ; Convert ASCII character to integer
-    movzx ebx, byte [rel num2] ; Load ASCII character of num2 into BL and zero-extend to EBX
-    sub ebx, '0'           ; Convert ASCII character to integer
-    add eax, ebx           ; Add the integers
-    add eax, '0'           ; Convert integer result back to ASCII character
-    mov [rel res], al      ; Store the result
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, [rel res]           ; Write result
-    mov edx, 1             ; We only write one byte
-    int 80h
-
-exit:
-	mov eax, 1
-	int 80h
-
 section .data
-	msg1 db "Enter num 1: "
-	msg1_len equ $ - msg1
-	msg2 db "Enter num 2: "
-	msg2_len equ $ - msg2
-	msg db "The sum is: "
-	msg_len equ $ - msg
+    prompt db 'Enter your name: ', 0    ; Prompt message
+    greeting db 'Hello, ', 0             ; Greeting message prefix
+    newline db 10, 0                     ; Newline character for printing
+
 section .bss
-	num1 resb 5 ; res 5 bytes
-	num2 resb 5
-	res resb 5
+    name resb 32                         ; Buffer to store user's name (up to 31 characters)
+
+section .text
+    global _start
+
+_start:
+    ; Display prompt message
+    mov rax, 1                  ; syscall number for sys_write
+    mov rdi, 1                  ; file descriptor 1 (stdout)
+    mov rsi, prompt             ; pointer to the prompt message
+    mov rdx, 17                 ; length of the prompt message
+    syscall                     ; invoke the system call
+
+    ; Read user input
+    mov rax, 0                  ; syscall number for sys_read
+    mov rdi, 0                  ; file descriptor 0 (stdin)
+    mov rsi, name               ; pointer to the buffer
+    mov rdx, 32                 ; maximum number of bytes to read
+    syscall                     ; invoke the system call
+
+    ; Print greeting
+    mov rax, 1                  ; syscall number for sys_write
+    mov rdi, 1                  ; file descriptor 1 (stdout)
+    mov rsi, greeting           ; pointer to the greeting prefix
+    mov rdx, 7                  ; length of the greeting prefix
+    syscall                     ; invoke the system call
+
+    ; Print the user's name
+    mov rax, 1                  ; syscall number for sys_write
+    mov rdi, 1                  ; file descriptor 1 (stdout)
+    mov rsi, name               ; pointer to the user's name
+    syscall                     ; invoke the system call
+
+    ; Print newline character
+    mov rax, 1                  ; syscall number for sys_write
+    mov rdi, 1                  ; file descriptor 1 (stdout)
+    mov rsi, newline            ; pointer to the newline character
+    mov rdx, 1                  ; length of the newline character
+    syscall                     ; invoke the system call
+
+    ; Exit the program
+    mov rax, 60                 ; syscall number for sys_exit
+    xor rdi, rdi                ; exit code 0
+    syscall                     ; invoke the system call
